e8a44b2c2dd24acdb41db01c0fae9e3ca52b1b3f
Kamil <kamilsalimgareev270@gmail.com>
2024-02-13 02:03:47 +0500

    Создана начальная страница

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const fetcher = (...args) => {fetch('https://dummyjson.com/users')
+  .then(res => res.json())
+  .then(console.log)};
+
+  fetcher();
+
   return (
-    <div className="App">
-      Hello!
+    <div className="wrapper">
+      <div className="container">
+        <h1>React v{React.version}</h1>
+        <input type="text" placeholder="Поиск элемента"/>
+      </div>
     </div>
   );
 }
