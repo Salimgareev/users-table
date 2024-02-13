b3147f637d14fc3c428d551fb90aa598e6707f8e
Kamil <kamilsalimgareev270@gmail.com>
2024-02-14 00:37:02 +0500

    Сделан мини-адаптив + почищен index.html

--- src/App.js
+++ src/App.js
@@ -35,23 +35,6 @@ function App() {
     }));
   };
 
-  // const theadData = ["Name", "Email", "Date"];
-
-  const tbodyData = [
-    {
-      id: "1",
-      items: ["John", "john@email.com", "01/01/2021"],
-    },
-    {
-      id: "2",
-      items: ["Sally", "sally@email.com", "12/24/2020"],
-    },
-    {
-      id: "3",
-      items: ["Maria", "maria@email.com", "12/01/2020"],
-    },
-  ];
-
   return (
     <div className="wrapper">
       <div className="container">
