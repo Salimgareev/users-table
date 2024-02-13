94f45425e5df365543634a3208760538aafeaa07
Kamil <kamilsalimgareev270@gmail.com>
2024-02-13 23:44:58 +0500

    Создан компонент таблицы, добавлены базовые стили к нему

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React from "react";
 import "./App.css";
+import Table from "./components/ui/Table";
 
 function App() {
-  const fetcher = (...args) => {fetch('https://dummyjson.com/users')
-  .then(res => res.json())
-  .then(console.log)};
+  
+
+  async function fetcher() {
+    try {
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
+      const mas = []; // создаем пустой массив
+
+      mas.push(data);
+
+      console.log(mas[0].users[0].lastName);
+      return mas // возвращаем массив для дальнейшего использования
+    } catch (error) {
+      console.log(error);
+      return null; // в случае ошибки возвращаем null
+    }
+  }
 
   fetcher();
+
+  const theadData = ["Name", "Email", "Date"];
+
+  const tbodyData = [
+    {
+      id: "1",
+      items: ["John", "john@email.com", "01/01/2021"],
+    },
+    {
+      id: "2",
+      items: ["Sally", "sally@email.com", "12/24/2020"],
+    },
+    {
+      id: "3",
+      items: ["Maria", "maria@email.com", "12/01/2020"],
+    },
+  ];
 
   return (
     <div className="wrapper">
       <div className="container">
         <h1>React v{React.version}</h1>
-        <input type="text" placeholder="Поиск элемента"/>
+        <input type="text" placeholder="Поиск элемента" />
+        <Table
+          theadData={theadData}
+          tbodyData={tbodyData}
+          customClass={"main-table"}
+        />
       </div>
     </div>
   );
